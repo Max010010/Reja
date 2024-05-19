d5197d48b19d03ea9ebd4b6ddde5564a3fbe7117
ismoil kosimov <ismoilkosimov@Ismoilui-MacBookPro.local>
2024-05-19 22:24:11 +0900

    feat: develope author portfolio

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
+console.log('Web servverni boshlash!');
+const express = require("express");
+const app = express();
+const http = require("http");
+const fs = require("fs");
 
-  console.log('Web servverni boshlash!');
-  const express = require("express");
-  const app = express();
-  const http = require("http");
-  
-  // 1 Kirish codelari
-  app.use(express.static("public"));
-  app.use(express.json());
-  app.use(express.urlencoded({extended: true}));
-  // 2: Session codelari
-  // 3 Views codelari
-   app.set("views", "views");
-   app.set("view engine", "ejs");
-  // 4 Root codelari
-app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test: "success"});
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
 });
 
-  app.get("/", function (req, res) {
-    res.render("harid")
-  });
-  
-  const server = http.createServer(app);
-  let PORT = 3000;
-  server.listen(PORT, function() {
-      console.log(`The server is running successfully on port: ${PORT}`);
-  });
+// 1 Kirish codelari
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+// 2: Session codelari
+// 3 Views codelari
+app.set("views", "views");
+app.set("view engine", "ejs");
+// 4 Root codelari
+
+app.get('/author', (req, res) => {
+    res.render("author", {
+        user: user
+    });
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function () {
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
