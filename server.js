4cbcc1889b33c80904fc2443e9d0fc4044287e3b
ismoil kosimov <ismoilkosimov@Ismoilui-MacBookPro.local>
2024-05-30 23:26:48 +0900

    Feat:Loyiha brainstorm va Flowchart & MITTSK-E

--- server.js
+++ server.js
@@ -2,24 +2,28 @@ const http = require("http");
 const mongodb = require("mongodb");
 
 let db;
-const connectionString = "mongodb+srv://Max:FM8rGHM7szPmLAAW@cluster0.ppsw31d.mongodb.net/Reja";
+const connectionString =
+  "mongodb+srv://Max:FM8rGHM7szPmLAAW@cluster0.ppsw31d.mongodb.net/Reja";
 
 mongodb.connect(
-    connectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    (err, client) => {
-        if (err) console.log("ERROR on connection MongoDB");
-        else {
-            console.log("MongoDB connection succed");
-            module.exports = client;
-            const app = require("./app");
-            const server = http.createServer(app);
-            let PORT = 3000;
-            server.listen(PORT, function () {
-                console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
-            });
-        }
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succed");
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
     }
+  }
 );
--- train.js
+++ train.js
@@ -1,28 +1,13 @@
-// D-TASK: 
+// E-TASK:
 
-// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
-// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+// Shunday function tuzing, u bitta string argumentni qabul qilib osha stringni teskari qilib return qilsin.
+// MASALAN: getReverse("hello") return qilsin "olleh"
 
-// Masalani yechimi 
+// Masalani yechimi
 
-let isAnagram = function (a, b) {
-  let freqMap = {};
+function getReverse(inputString) {
+  return inputString.split("").reverse().join("");
+}
+let result = getReverse("hello");
 
-  for (let letter of a) {
-    freqMap[letter] = (freqMap[letter] || 0) + 1;
-  }
-
-  for (let bLetter of b) {
-    if (!freqMap[bLetter] || freqMap[bLetter] === 0) {
-      return false;
-    } else {
-      freqMap[bLetter]--;
-    }
-  }
-  return true;
-};
-
-let a = "mitgroup";
-let b = "igmtprou";
-
-console.log(isAnagram(a, b));
+console.log(result);
